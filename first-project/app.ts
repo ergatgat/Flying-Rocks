2fb3a5dd3e6b7d973de79bd31d9a6466401f0cf2
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-03-11 15:26:04 +0200

    push

--- first-project/app.ts
+++ first-project/app.ts
@@ -1,1 +1,36 @@
-console.log('works!')
+const meteors:NodeList = document.querySelectorAll('.meteor')
+
+// function gets meteors and returns each meteor position
+function getMeteorPosition(meteors:NodeList):object{
+    const ourMeteors:Array<object> = [];
+    meteors.forEach(element => {
+        const meteor = {
+            leftPosition: element.getBoundingClientRect().left,
+            rightPosition: element.getBoundingClientRect().right,
+            topPosition: element.getBoundingClientRect().top,
+            bottomPosition: element.getBoundingClientRect().bottom,
+        }
+        ourMeteors.push(meteor)
+    })
+    return ourMeteors;
+}
+
+function moveMeteors(meteors:NodeList){
+    const bodyWidth = document.body.offsetWidth;
+    const bodyHeight = document.body.offsetHeight;
+    console.log(`bodyWidth: ${bodyWidth}, bodyHeight:${bodyHeight}`)
+    meteors.forEach(element => {
+        element.style.top = getRandomNumber(bodyHeight)+`px`
+        element.style.left = getRandomNumber(bodyWidth)+`px`
+    })
+}
+
+function getRandomNumber(max:number):number{
+    let randNumber = Math.floor(Math.random() * max);
+    if(randNumber > 100){
+        randNumber -= 100;
+    }
+    console.log(randNumber)
+    return randNumber;
+}
+
--- first-project/dist/app.js
+++ first-project/dist/app.js
@@ -1,1 +1,32 @@
-console.log('works!');
+var meteors = document.querySelectorAll('.meteor');
+// function gets meteors and returns each meteor position
+function getMeteorPosition(meteors) {
+    var ourMeteors = [];
+    meteors.forEach(function (element) {
+        var meteor = {
+            leftPosition: element.getBoundingClientRect().left,
+            rightPosition: element.getBoundingClientRect().right,
+            topPosition: element.getBoundingClientRect().top,
+            bottomPosition: element.getBoundingClientRect().bottom
+        };
+        ourMeteors.push(meteor);
+    });
+    return ourMeteors;
+}
+function moveMeteors(meteors) {
+    var bodyWidth = document.body.offsetWidth;
+    var bodyHeight = document.body.offsetHeight;
+    console.log("bodyWidth: " + bodyWidth + ", bodyHeight:" + bodyHeight);
+    meteors.forEach(function (element) {
+        element.style.top = getRandomNumber(bodyHeight) + "px";
+        element.style.left = getRandomNumber(bodyWidth) + "px";
+    });
+}
+function getRandomNumber(max) {
+    var randNumber = Math.floor(Math.random() * max);
+    if (randNumber > 100) {
+        randNumber -= 100;
+    }
+    console.log(randNumber);
+    return randNumber;
+}
