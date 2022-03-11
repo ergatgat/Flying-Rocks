e99f842718a255a855624c72daffb2327e55f0b1
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-03-11 19:01:18 +0200

    EASY TASK
    i dont know why everybody always complaining about int hello iam tal yaron from int

--- first-project/app.ts
+++ first-project/app.ts
@@ -1,8 +1,10 @@
 const meteors:NodeList = document.querySelectorAll('.meteor')
 
+const body:HTMLBodyElement = document.querySelector('body')
+
 // function gets meteors and returns each meteor position
-function getMeteorPosition(meteors:NodeList):object{
-    const ourMeteors:Array<object> = [];
+function getMeteorPosition(meteors: NodeList): object {
+    const ourMeteors: Array<object> = [];
     meteors.forEach(element => {
         const meteor = {
             leftPosition: element.getBoundingClientRect().left,
@@ -15,22 +17,68 @@ function getMeteorPosition(meteors:NodeList):object{
     return ourMeteors;
 }
 
-function moveMeteors(meteors:NodeList){
+// function moves meteors individually
+function moveMeteors() {
+    setInterval(checkCollision,100)
     const bodyWidth = document.body.offsetWidth;
     const bodyHeight = document.body.offsetHeight;
-    console.log(`bodyWidth: ${bodyWidth}, bodyHeight:${bodyHeight}`)
     meteors.forEach(element => {
-        element.style.top = getRandomNumber(bodyHeight)+`px`
-        element.style.left = getRandomNumber(bodyWidth)+`px`
+        element.style.top = getRandomNumber(bodyHeight) + `px`
+        element.style.left = getRandomNumber(bodyWidth) + `px`
     })
 }
 
-function getRandomNumber(max:number):number{
+function checkCollision() {
+    // Check if meteor[i] collid meteor[i+1]
+    const meteorPosition: Array<object> = getMeteorPosition(meteors)
+    for (let i = 0; i < meteorPosition.length; i++) {
+        for (let j = 1; j < meteorPosition.length; j++) {
+            if (i === j) {
+                break;
+            } else if (meteorPosition[i].leftPosition >= meteorPosition[j].leftPosition &&
+                meteorPosition[i].leftPosition <= meteorPosition[j].rightPosition) {
+                if (meteorPosition[i].topPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].topPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+                        
+
+                } else if (meteorPosition[i].bottomPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].bottomPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+
+                }
+            } else if (meteorPosition[i].rightPosition >= meteorPosition[j].leftPosition &&
+                meteorPosition[i].rightPosition <= meteorPosition[j].rightPosition) {
+                if (meteorPosition[i].bottomPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].bottomPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+
+
+                } else if (meteorPosition[i].topPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].topPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+
+
+                }
+            } else {
+                console.log('no overlap')
+            }
+
+        }
+    }
+}
+
+function getRandomNumber(max: number): number {
     let randNumber = Math.floor(Math.random() * max);
-    if(randNumber > 100){
+    if (randNumber > 100) {
         randNumber -= 100;
     }
-    console.log(randNumber)
     return randNumber;
 }
+
+setInterval(moveMeteors,5000)
 
--- first-project/dist/app.js
+++ first-project/dist/app.js
@@ -1,4 +1,5 @@
 var meteors = document.querySelectorAll('.meteor');
+var body = document.querySelector('body');
 // function gets meteors and returns each meteor position
 function getMeteorPosition(meteors) {
     var ourMeteors = [];
@@ -13,20 +14,61 @@ function getMeteorPosition(meteors) {
     });
     return ourMeteors;
 }
-function moveMeteors(meteors) {
+// function moves meteors individually
+function moveMeteors() {
+    setInterval(checkCollision, 100);
     var bodyWidth = document.body.offsetWidth;
     var bodyHeight = document.body.offsetHeight;
-    console.log("bodyWidth: " + bodyWidth + ", bodyHeight:" + bodyHeight);
     meteors.forEach(function (element) {
         element.style.top = getRandomNumber(bodyHeight) + "px";
         element.style.left = getRandomNumber(bodyWidth) + "px";
     });
+}
+function checkCollision() {
+    // Check if meteor[i] collid meteor[i+1]
+    var meteorPosition = getMeteorPosition(meteors);
+    for (var i = 0; i < meteorPosition.length; i++) {
+        for (var j = 1; j < meteorPosition.length; j++) {
+            if (i === j) {
+                break;
+            }
+            else if (meteorPosition[i].leftPosition >= meteorPosition[j].leftPosition &&
+                meteorPosition[i].leftPosition <= meteorPosition[j].rightPosition) {
+                if (meteorPosition[i].topPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].topPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(i + " and " + j + " Are overlapping at X and Y");
+                }
+                else if (meteorPosition[i].bottomPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].bottomPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(i + " and " + j + " Are overlapping at X and Y");
+                }
+            }
+            else if (meteorPosition[i].rightPosition >= meteorPosition[j].leftPosition &&
+                meteorPosition[i].rightPosition <= meteorPosition[j].rightPosition) {
+                if (meteorPosition[i].bottomPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].bottomPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(i + " and " + j + " Are overlapping at X and Y");
+                }
+                else if (meteorPosition[i].topPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].topPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(i + " and " + j + " Are overlapping at X and Y");
+                }
+            }
+            else {
+                console.log('no overlap');
+            }
+        }
+    }
 }
 function getRandomNumber(max) {
     var randNumber = Math.floor(Math.random() * max);
     if (randNumber > 100) {
         randNumber -= 100;
     }
-    console.log(randNumber);
     return randNumber;
 }
+setInterval(moveMeteors, 5000);
