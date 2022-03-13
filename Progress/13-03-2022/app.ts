11a0ba711e656cc4f24c76f398bb6eaf8df968a7
GiliEllen <gilimena95@gmail.com>
2022-03-13 19:54:14 +0200

    שיניייים
    השמיים שלך צמאים
    בשינויים

--- Progress/13-03-2022/app.ts
+++ Progress/13-03-2022/app.ts
@@ -1,6 +1,6 @@
 const meteors: NodeList = document.querySelectorAll('.meteor')
-const spaceship = document.querySelector('#spacShip')
-
+const spaceship:HTMLDivElement = document.querySelector('#spaceShip')
+const body:HTMLBodyElement = document.querySelector('body')
 const wrapper: HTMLDivElement = document.querySelector('.wrapper')
 
 // function gets meteors and returns each meteor position
@@ -158,44 +158,15 @@ function setRandomRotate() {
     })
 }
 
+body.addEventListener('mousemove', (ev) => {
+    const middleElementX = ev.clientX
+    const middleElementY = ev.clientY
+    spaceship.style.top = `${middleElementY}px`
+    spaceship.style.left = `${middleElementX}px`
+}) 
+
 moveMeteors();
 setTimeout(moveMeteors, 500)
 setRandomRotate();
 setInterval(moveMeteors, 5000)
 
-dragElement(document.getElementById("spaceShip"));
-
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        elmnt.onmousedown = dragMouseDown;
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
--- Progress/13-03-2022/dist/app.js
+++ Progress/13-03-2022/dist/app.js
@@ -1,5 +1,6 @@
 var meteors = document.querySelectorAll('.meteor');
-var spaceship = document.querySelector('#spacShip');
+var spaceship = document.querySelector('#spaceShip');
+var body = document.querySelector('body');
 var wrapper = document.querySelector('.wrapper');
 // function gets meteors and returns each meteor position
 function getMeteorPosition(meteors) {
@@ -151,39 +152,13 @@ function setRandomRotate() {
         element.style.transform = "rotate(" + randomRotate + "deg)";
     });
 }
+body.addEventListener('mousemove', function (ev) {
+    var middleElementX = ev.clientX;
+    var middleElementY = ev.clientY;
+    spaceship.style.top = middleElementY + "px";
+    spaceship.style.left = middleElementX + "px";
+});
 moveMeteors();
 setTimeout(moveMeteors, 500);
 setRandomRotate();
 setInterval(moveMeteors, 5000);
-dragElement(document.getElementById("spaceShip"));
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    elmnt.onmousedown = dragMouseDown;
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
