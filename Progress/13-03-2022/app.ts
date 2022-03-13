e945dbbd4143e2c3414b7231efec0fb7ad523bf7
GiliEllen <gilimena95@gmail.com>
2022-03-13 19:37:09 +0200

    hello
    its me

---
+++ Progress/13-03-2022/app.ts
@@ -0,0 +1,201 @@
+const meteors: NodeList = document.querySelectorAll('.meteor')
+const spaceship = document.querySelector('#spacShip')
+
+const wrapper: HTMLDivElement = document.querySelector('.wrapper')
+
+// function gets meteors and returns each meteor position
+function getMeteorPosition(meteors: NodeList): object {
+    const ourMeteors: Array<object> = [];
+    meteors.forEach(element => {
+        const meteor = {
+            leftPosition: element.getBoundingClientRect().left,
+            rightPosition: element.getBoundingClientRect().right,
+            topPosition: element.getBoundingClientRect().top,
+            bottomPosition: element.getBoundingClientRect().bottom,
+        }
+        ourMeteors.push(meteor)
+    })
+    return ourMeteors;
+}
+
+// function moves meteors individually
+function moveMeteors() {
+    setInterval(checkCollision, 100)
+    const bodyWidth = document.body.offsetWidth;
+    const bodyHeight = document.body.offsetHeight;
+    meteors.forEach(element => {
+        element.style.top = getRandomNumber(bodyHeight) + `px`
+        element.style.left = getRandomNumber(bodyWidth) + `px`
+    })
+}
+
+function checkCollision() {
+    // Check if meteor[i] collid meteor[i+1]
+    const meteorPosition: Array<object> = getMeteorPosition(meteors)
+    let isExpolded: boolean = false
+    for (let i = 0; i < meteorPosition.length; i++) {
+        for (let j = 1; j < meteorPosition.length; j++) {
+            if (i === j) {
+                break;
+            } else if (typeof i === 'undefined') {
+                break
+            } else if (meteorPosition[i].leftPosition >= meteorPosition[j].leftPosition &&
+                meteorPosition[i].leftPosition <= meteorPosition[j].rightPosition) {
+                if (meteorPosition[i].topPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].topPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+
+
+                    if (!isExpolded) {
+                        isExpolded = true;
+                        const explosionPositionTop = (meteorPosition[i].topPosition + meteorPosition[j].topPosition) / 2;
+                        const explosionPositionLeft = (meteorPosition[i].leftPosition + meteorPosition[j].leftPosition) / 2;
+
+                        const newExplostion = document.createElement('img')
+                        newExplostion.classList.add('explostion')
+                        newExplostion.src = "images/explosion-transparent-png-picture-1.png";
+                        newExplostion.style.top = `${explosionPositionTop}px`
+                        newExplostion.style.left = `${explosionPositionLeft}px`
+                        wrapper.appendChild(newExplostion);
+                        setTimeout(() => {
+                            newExplostion.remove()
+                            isExpolded = false;
+                        }, 100)
+                        meteors[i].removeChild
+                        meteors[j].removeChild
+                    }
+
+                } else if (meteorPosition[i].bottomPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].bottomPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+
+                    if (!isExpolded) {
+                        isExpolded = true;
+                        const explosionPositionTop = (meteorPosition[i].topPosition + meteorPosition[j].topPosition) / 2;
+                        const explosionPositionLeft = (meteorPosition[i].leftPosition + meteorPosition[j].leftPosition) / 2;
+
+                        const newExplostion = document.createElement('img')
+                        newExplostion.classList.add('explostion')
+                        newExplostion.src = "images/explosion-transparent-png-picture-1.png";
+                        newExplostion.style.top = `${explosionPositionTop}px`
+                        newExplostion.style.left = `${explosionPositionLeft}px`
+                        wrapper.appendChild(newExplostion);
+                        setTimeout(() => {
+                            newExplostion.remove()
+                            isExpolded = false;
+                        }, 100)
+                    }
+                }
+            } else if (meteorPosition[i].rightPosition >= meteorPosition[j].leftPosition &&
+                meteorPosition[i].rightPosition <= meteorPosition[j].rightPosition) {
+                if (meteorPosition[i].bottomPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].bottomPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+                    if (!isExpolded) {
+                        isExpolded = true;
+                        const explosionPositionTop = (meteorPosition[i].topPosition + meteorPosition[j].topPosition) / 2;
+                        const explosionPositionLeft = (meteorPosition[i].leftPosition + meteorPosition[j].leftPosition) / 2;
+
+                        const newExplostion = document.createElement('img')
+                        newExplostion.classList.add('explostion')
+                        newExplostion.src = "images/explosion-transparent-png-picture-1.png";
+                        newExplostion.style.top = `${explosionPositionTop}px`
+                        newExplostion.style.left = `${explosionPositionLeft}px`
+                        wrapper.appendChild(newExplostion);
+                        setTimeout(() => {
+                            newExplostion.remove()
+                            isExpolded = false;
+                        }, 100)
+                    }
+
+                } else if (meteorPosition[i].topPosition >= meteorPosition[j].topPosition &&
+                    meteorPosition[i].topPosition <= meteorPosition[j].bottomPosition) {
+                    // Add Explostion and Remove divs
+                    console.log(`${i} and ${j} Are overlapping at X and Y`)
+
+                    if (!isExpolded) {
+                        isExpolded = true;
+                        const explosionPositionTop = (meteorPosition[i].topPosition + meteorPosition[j].topPosition) / 2;
+                        const explosionPositionLeft = (meteorPosition[i].leftPosition + meteorPosition[j].leftPosition) / 2;
+
+                        const newExplostion = document.createElement('img')
+                        newExplostion.classList.add('explostion')
+                        newExplostion.src = "images/explosion-transparent-png-picture-1.png";
+                        newExplostion.style.top = `${explosionPositionTop}px`
+                        newExplostion.style.left = `${explosionPositionLeft}px`
+                        wrapper.appendChild(newExplostion);
+                        setTimeout(() => {
+                            newExplostion.remove()
+                            isExpolded = false;
+                        }, 100)
+                    }
+                }
+            } else {
+                console.log('no overlap')
+            }
+
+        }
+    }
+}
+
+// Function getting random number to set width and height of meteor postion top,left
+function getRandomNumber(max: number): number {
+    let randNumber = Math.floor(Math.random() * max);
+    if (randNumber > 100) {
+        randNumber -= 100;
+    }
+    return randNumber;
+}
+
+// Function sets random rotate for each meteor
+function setRandomRotate() {
+    meteors.forEach(element => {
+        const randomRotate = Math.floor(Math.random() * 360)
+        element.style.transform = `rotate(${randomRotate}deg)`
+    })
+}
+
+moveMeteors();
+setTimeout(moveMeteors, 500)
+setRandomRotate();
+setInterval(moveMeteors, 5000)
+
+dragElement(document.getElementById("spaceShip"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
